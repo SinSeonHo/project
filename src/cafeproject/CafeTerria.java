aec285909c9a87146543dcdb0692d43a4551c150
Kimgeunbae <bb009123@gmail.com>
2025-03-25 11:16:16 +0900

    수정

--- src/cafeproject/CafeTerria.java
+++ src/cafeproject/CafeTerria.java
@@ -4,6 +4,7 @@ public class CafeTerria {
     public static void main(String[] args) {
         Lunacafe cafe1 = new Lunacafe("김씨", null, 0);
         Mooncafe cafe2 = new Mooncafe("이씨", null, 0);
+        System.out.println("실험");
 
     }
 
