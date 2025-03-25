45c6a0c77560e1aefa7fb8994a275132844d3d1d
donghyun <okdh0329@gmail.com>
2025-03-25 10:48:59 +0900

    ssh 테스트파일 수정 추가

--- src/pushTest.java
+++ src/pushTest.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class pushTest {
     public static void main(String[] args) {
         System.out.println("ssh push test");
+
+        // 수정해서 해보겠습니다.
+        System.out.println("강동현 테스트 추가");
+        Scanner input = new Scanner(System.in);
+        String a = input.nextLine();
+        System.out.println(a);
     }
 }
