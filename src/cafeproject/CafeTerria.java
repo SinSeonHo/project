94a008beb90304b32572c97bb4cf5a6eceda6c97
Kimgeunbae <bb009123@gmail.com>
2025-03-25 11:12:40 +0900

    Cafe

--- src/cafeproject/CafeTerria.java
+++ src/cafeproject/CafeTerria.java
@@ -2,6 +2,8 @@ package cafeproject;
 
 public class CafeTerria {
     public static void main(String[] args) {
+        Lunacafe cafe1 = new Lunacafe(null, null, 0);
+        Mooncafe cafe2 = new Mooncafe(null, null, 0);
 
     }
 
