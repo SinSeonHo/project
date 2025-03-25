cc606004d87b65e35aac232f5b177818feb138bd
SinSeonHo <loop63@naver.com>
2025-03-25 11:14:02 +0900

    1~100출력추가

---
+++ src/abc/asdf.java
@@ -0,0 +1,9 @@
+package abc;
+
+public class asdf {
+    public static void main(String[] args) {
+        for (int i = 1; i < 101; i++) {
+            System.out.printf("%d ", i);
+        }
+    }
+}
