996fe190492048b8cc7a958f0d63ef1dd2c63a9c
Kimgeunbae <bb009123@gmail.com>
2025-03-25 11:07:11 +0900

    오류수정

--- src/pushTest.java
+++ src/pushTest.java
@@ -10,5 +10,14 @@ public class pushTest {
         String a = input.nextLine();
         System.out.println(a);
         System.out.println("test2");
+        // 테스트 문구입니다.
+        System.out.println("테스트 시작");
+        String test = new String();
+        Scanner scan = new Scanner(System.in);
+        int players = scan.nextInt();
+        System.out.print("  ");
+        for (int i = 0; i < players; i++) {
+            System.out.printf("%s   ", i + 1);
+        }
     }
 }
